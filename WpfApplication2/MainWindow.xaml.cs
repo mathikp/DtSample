379d1926247b46146286333fe6f464aaa4130043
Mathivanan KP <mathikp@yahoo.com>
2018-07-04 17:54:27 +0530

    Initial commit

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Journal dc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dc = new Journal();
+            myPanel.DataContext = dc;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dc.Narration = "clicked";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dc = new Journal();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            dc.Narration = "updated";
+        }
+    }
+
+    public class Journal : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _Narration;
+
+        public string Narration
+        {
+            get
+            {
+                return _Narration;
+            }
+            set
+            {
+                _Narration = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
